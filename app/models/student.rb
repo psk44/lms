ebd43cfe025c8127bcfb5c4e349a7594f9e005dd
Laney Stroup <tstroup@masters.edu>
2023-09-28 02:32:42 +0000

    add thumb variant to profile picture for use in student views;

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
-  has_one_attached :profile_picture
+  has_one_attached :profile_picture do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
 
 end

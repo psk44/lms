5567405680fe6b45163514147a410323fc601029
Laney Stroup <tstroup@masters.edu>
2023-10-03 01:13:08 +0000

    run migration;

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_03_010602) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_03_011121) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,6 +42,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_03_010602) do
   create_table "courses", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "senseis", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

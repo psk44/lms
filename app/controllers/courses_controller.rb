0ac397ac3d73707f2070eb03fab2cbea96634170
Laney Stroup <tstroup@masters.edu>
2023-10-05 00:52:44 +0000

    add sensei to controller and views;

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -65,6 +65,6 @@ class CoursesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def course_params
-      params.require(:course).permit(:title, :description)
+      params.require(:course).permit(:title, :description, :sensei_id)
     end
 end

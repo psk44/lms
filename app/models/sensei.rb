cb8b40be9f4f7774c8f9703370fbaa13ce15f1dc
Laney Stroup <tstroup@masters.edu>
2023-10-12 01:32:00 +0000

    add compliant? to sensei model;

--- app/models/sensei.rb
+++ app/models/sensei.rb
@@ -5,4 +5,12 @@ class Sensei < ApplicationRecord
   def full_name
     first_name + " " + last_name
   end
+
+  def max_student_count
+    1
+  end
+
+  def compliant?
+    students.count <= max_student_count
+  end
 end

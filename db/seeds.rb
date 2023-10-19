018492e5ab605a47e40e0faadf3f52782df5e140
Laney Stroup <tstroup@masters.edu>
2023-10-19 06:48:27 +0000

    populate seeds.rb file;

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+sensei = Sensei.create({
+  :first_name => "Gandalf",
+  :last_name => "The Wise"
+})
+
+student_hashes = [
+  {
+    :first_name => "Bilbo",
+    :last_name => "Baggins"
+  },
+  {
+    :first_name => "Frodo",
+    :last_name => "Baggins"
+  },
+  {
+    :first_name => "Pippin",
+    :last_name => "Took"
+  }
+]
+
+Student.create(student_hashes)
+10.times do |index|
+  course = Course.create(
+    :title => "Title #{index}",
+    :description => "Description #{index}",
+    :sensei => sensei
+  )
+
+  course.students << Student.find(rand(1..3))
+end

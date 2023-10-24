5887e80159812f49b1f3978e1c8e9000c4e318f5
Laney Stroup <tstroup@masters.edu>
2023-10-24 00:49:28 +0000

    scaffold HTML for search form;

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :senseis
   resources :courses
   resources :students
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get 'search', :to => "search#index"
 
   # Defines the root path route ("/")
   root "dashboard#index"

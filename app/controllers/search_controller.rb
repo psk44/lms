1040b8edc54f4127c6d0fdf03f6c07eb3c5651fd
Laney Stroup <tstroup@masters.edu>
2023-10-26 00:39:51 +0000

    scaffold sensei_id filter;

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   def index
     @query = params[:query]
+    @sensei_id = params[:sensei_id]
 
-    @courses = Course.where("title like '%#{@query}%'")
+    @courses = Course.where("title like '%#{@query}%'").where(sensei_id: @sensei_id)
   end
 end

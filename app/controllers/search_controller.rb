16d9a40caed5e182bc3b5936437b711fe0174dd9
Laney Stroup <tstroup@masters.edu>
2023-10-26 01:12:17 +0000

    make sensei_id optional in controller;

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,10 @@ class SearchController < ApplicationController
     @query = params[:query]
     @sensei_id = params[:sensei_id]
 
-    @courses = Course.where("title like '%#{@query}%'").where(sensei_id: @sensei_id)
+    if @sensei_id.present?
+      @courses = Course.where("title like '%#{@query}%'").where(sensei_id: @sensei_id)
+    else
+      @courses = Course.where("title like '%#{@query}%'")
+    end
   end
 end
